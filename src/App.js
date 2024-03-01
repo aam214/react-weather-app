94fc0119107696b1f3eedd8a55fc80ea0ecd6cc4
Ariana <ariana.morin214@gmail.com>
2024-02-29 18:22:40 -0600

    Added footer with link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return;
+  <div className="App">Weather App</div>;
+  <footer>
+    {" "}
+    This project was coded by Ariana Morin and is open-sourced on
+    <a href="https://github.com/aam214/react-weather-app." target="_blank">
+      GitHub
+    </a>
+  </footer>;
 }
