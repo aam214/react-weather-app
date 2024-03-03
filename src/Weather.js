db0e40d40fecf18db512cd29a843b1edaa09e80f
Ariana <ariana.morin214@gmail.com>
2024-03-02 20:19:37 -0600

    Changed HTML row and column template to make 4 sections in a square. Changed font-size and font-weight.

--- src/Weather.js
+++ src/Weather.js
@@ -5,43 +5,40 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a City..."
-              className="search-box"
-              autoFocus="on"
-            />
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary ms-3"
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          className="search-box"
+          autoFocus="on"
+        />
+        <input type="submit" value="Search" className=" ms-2 btn btn-primary" />
+      </form>
+      <div class="row">
+        <div class="col-6">
+          <h1>New York</h1>
+          <div className=".date-condition">Saturday 10:30 PM</div>
+        </div>
+        <div class="col-6">
+          <div class="temperature">
+            10<span className="unit">°C</span>
+          </div>
+        </div>
+        <div class="col-6">
+          <div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="Rainy icon"
             />
           </div>
         </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Thursday 8:39PM</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="Mostly Cloudy"
-          />
-
-          <span className="temperature">10</span>
-          <span className="unit">°C</span>
-        </div>
-        <div col-md-6>
-          <ul className="readings">
-            <li>Precipitation: 5%</li>
-            <li>Humidity: 20%</li>
-            <li>Wind: 10 km/h</li>
-          </ul>
+        <div class="col-6">
+          <div class="readings">
+            <ul>
+              <li>Wind: 15 km/h</li>
+              <li>Humidity: 10%</li>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
