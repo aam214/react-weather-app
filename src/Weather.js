6fa44082aa4ec4d8ef5e85fdf8a14e787076a012
Ariana <ariana.morin214@gmail.com>
2024-03-02 20:38:18 -0600

    Added body element, changed color of border and texts.

--- src/Weather.js
+++ src/Weather.js
@@ -3,44 +3,50 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <form>
-        <input
-          type="search"
-          placeholder="Enter a city.."
-          className="search-box"
-          autoFocus="on"
-        />
-        <input type="submit" value="Search" className=" ms-2 btn btn-primary" />
-      </form>
-      <div class="row">
-        <div class="col-6">
-          <h1>New York</h1>
-          <h4 className=".date-condition">Saturday 10:30 PM</h4>
-        </div>
-        <div class="col-6">
-          <div class="temperature">
-            10<span className="unit">°C</span>
+    <body>
+      <div className="Weather">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="search-box"
+            autoFocus="on"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className=" ms-2 btn btn-primary"
+          />
+        </form>
+        <div class="row">
+          <div class="col-6">
+            <h1>New York</h1>
+            <h4 className=".date-condition">Saturday 10:30 PM</h4>
           </div>
-        </div>
-        <div class="col-6">
-          <div>
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-              alt="Rainy icon"
-            />
+          <div class="col-6">
+            <div class="temperature">
+              10<span className="unit">°C</span>
+            </div>
           </div>
-        </div>
-        <div class="col-6">
-          <div class="readings">
-            <ul>
-              <li>Wind: 15 km/h</li>
-              <li>Humidity: 10%</li>
-              <li></li>
-            </ul>
+          <div class="col-6">
+            <div>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                alt="Rainy icon"
+              />
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="readings">
+              <ul>
+                <li>Precipitation: 20%</li>
+                <li>Wind: 15 km/h</li>
+                <li>Humidity: 10%</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </body>
   );
 }
