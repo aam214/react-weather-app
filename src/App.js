140096b3bb1534acad82ed350e96e9b3ec24972d
Ariana <152944814+aam214@users.noreply.github.com>
2024-03-17 14:36:12 -0500

    Updated GitHub link in footer.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <footer>
         This project was coded by Ariana Morin and is open-sourced on
         <a
-          href="https://github.com/aam214/react-weather-app."
+          href="https://github.com/aam214/react-weather-app"
           target="_blank"
           rel="noreferrer"
         > {""}GitHub
